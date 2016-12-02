e02145fb94546e00b36519426b78c374a4a8fb2c
Jon Cinque <jon.cinque@gmail.com>
2016-12-02 17:24:34 +0000

    Forbid new account creation, only admin account

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,5 +1,9 @@
 import { Accounts } from 'meteor/accounts-base';
 
+Accounts.config({ 
+  forbidClientAccountCreation: true,
+});
+
 Accounts.ui.config({
   passwordSignupFields: 'EMAIL_ONLY', 
     // can be customized for different kinds of fields, including FB integration
