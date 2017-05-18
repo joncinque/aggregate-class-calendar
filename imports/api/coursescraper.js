7bbf6042b4ce6e4994b1c6bd44268307fa57c76f
Jon Cinque <jon.cinque@gmail.com>
2017-05-18 17:44:12 +0100

    Add Chrome scraper and adding by file to admin

--- imports/api/coursescraper.js
+++ imports/api/coursescraper.js
@@ -1,9 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
+import { EventEmitter } from 'events';
+
 import phantomjs from 'phantomjs-prebuilt';
 
-import { parsePage } from './parsecourse.js';
+import { parsePage, makeParsePageEventEmitter } from './parsecourse.js';
 import { Courses } from './courses.js';
 
 function logCourse(course)
@@ -19,6 +21,13 @@ function logCourse(course)
   logger.info("  , style: '" + course.style + "'");
   logger.info("  , postcode: '" + course.postcode + "'");
   logger.info("}");
+}
+
+function addJSONCourseToDB(course)
+{
+  course.start = new Date(course.start);
+  course.end = new Date(course.end);
+  Meteor.call('courses.insert', course);
 }
 
 function addCoursesToDB(courses)
@@ -87,8 +96,20 @@ function getCoursesAsync(studio, callback)
   }
 }
 
+function getCoursesChrome(studio, callback)
+{
+  const htmlFile = Math.abs(studio.studioid) + '.html';
+  return chromegetcourse.dumpCourseTable(
+      studio.provider,
+      studio.studioid,
+      studio.redirectPage
+      ).once('finish-dumping',
+        parsecourse.makeParsePageEventEmitter(studio, callback));
+}
+
+
 Meteor.methods({
-  'coursescraper.getAllCourses'()
+  'coursescraper.getAllCoursesPhantom'()
   {
     if (Meteor.isServer)
     {
@@ -111,7 +132,7 @@ Meteor.methods({
       return Promise.resolve([]);
     }
   },
-  async 'coursescraper.getAllCoursesSync' ()
+  async 'coursescraper.getAllCoursesSyncPhantom' ()
   {
     if (Meteor.isServer)
     {
@@ -128,26 +149,93 @@ Meteor.methods({
     }
     return Promise.resolve([]);
   },
-  'coursescraper.getCourses'(studioid)
+  async 'coursescraper.getAllCoursesChrome' ()
   {
-    check(studioid, Number);
     if (Meteor.isServer)
     {
       let studioInfo = JSON.parse(Assets.getText('studios.json'));
+      let boundAddCourses = Meteor.bindEnvironment(addCoursesToDB);
+      /*
       for (let index in studioInfo)
       {
         let studio = studioInfo[index];
-        if (studio.studioid === studioid)
-        {
+        let courses = await getCoursesAsync(studio);
+        boundAddCourses(courses);
+        logger.info('Finished for studio: ' + studio.name);
+      }
+      logger.info('Scraping all done');
+      */
+    }
+    return Promise.resolve([]);
+  },
+  async 'coursescraper.addCoursesFromFile' (jsonFile)
+  {
+    if (Meteor.isServer)
+    {
+      let courses = JSON.parse(Assets.getText(jsonFile));
+      courses.forEach(addJSONCourseToDB);
+    }
+    return Promise.resolve([]);
+  },
+  'coursescraper.getCoursesPhantom'(studioid)
+  {
+    check(studioid, Number);
+    if (Meteor.isServer) {
+      let studioInfo = JSON.parse(Assets.getText('studios.json'));
+      for (let index in studioInfo) {
+        let studio = studioInfo[index];
+        if (studio.studioid === studioid) {
           return getCoursesAsync(
               studio,
               Meteor.bindEnvironment(makeDBCallback(studio)));
         }
       }
-    }
-    else
-    {
+    } else {
       return Promise.resolve([]);
     }
+  },
+  'coursescraper.getCoursesChrome'(studioid)
+  {
+    check(studioid, Number);
+    let ee = new EventEmitter();
+    if (Meteor.isServer) {
+      let studioInfo = JSON.parse(Assets.getText('studios.json'));
+      for (let index in studioInfo) {
+        let studio = studioInfo[index];
+        if (studio.studioid === studioid) {
+          getCoursesChrome(
+              studio,
+              Meteor.bindEnvironment(makeDBCallback(studio))
+            ).once('finish-scraping', (data)=>{
+              ee.emit('finish-all-scraping');
+          });
+        }
+      }
+    }
+    return ee;
+  },
+  'coursescraper.getAllCoursesChrome'(studioid)
+  {
+    let ee = new EventEmitter();
+    if (Meteor.isServer) {
+      let studioInfo = JSON.parse(Assets.getText('studios.json'));
+      ee.on('finish-studio', (index)=>{
+        if (index < studioInfo.length) {
+          let studio = studioInfo[index];
+          getCoursesChrome(
+              studio,
+              Meteor.bindEnvironment(makeDBCallback(studio))
+              ).once('finish-scraping', (data)=>{
+                ee.emit('finish-studio', ++index);
+              }).once('error', (data)=>{
+                ee.emit('finish-studio', ++index);
+              });
+        } else {
+          ee.emit('finish-all-scraping');
+          logger.info('Scraping complete');
+        }
+      });
+    }
+    return ee;
   },
 });
--- imports/ui/scraper.js
+++ imports/ui/scraper.js
@@ -4,30 +4,67 @@ import { Template } from 'meteor/templating';
 import './scraper.html';
 
 Template.scraper.events({
-  'click .scrape-all'() {
-    Meteor.call('coursescraper.getAllCourses', (err, data) => {
+  'click .scrape-all-phantom'() {
+    Meteor.call('coursescraper.getAllCoursesPhantom', (err, data) => {
       if (err) {
         console.log(err);
       }
     });
   },
-  'click .scrape-all-sync'() {
-    Meteor.call('coursescraper.getAllCoursesSync', (err, data) => {
+  'click .scrape-all-sync-phantom'() {
+    Meteor.call('coursescraper.getAllCoursesSyncPhantom', (err, data) => {
       if (err) {
         console.log(err);
       }
     });
   },
-  'submit .scrape-mbo-page'(event) {
+  'submit .scrape-mbo-page-phantom'(event) {
     // Prevent default browser form submit
     event.preventDefault();
     const studioid = Number(event.target.studioid.value);
     if (studioid !== 0) {
-      Meteor.call('coursescraper.getCourses', studioid, (err, data) => {
+      Meteor.call('coursescraper.getCoursesPhantom', studioid, (err, data) => {
         if (err) {
           console.log(err);
         } else {
           console.log('Finished single scrape');
+        }
+      });
+    }
+  },
+  'submit .scrape-all-sync-chrome'() {
+    Meteor.call('coursescraper.getAllCoursesChrome', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Finished single scrape');
+      }
+    });
+  },
+  'submit .scrape-mbo-page-chrome'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+    const studioid = Number(event.target.studioid.value);
+    if (studioid !== 0) {
+      Meteor.call('coursescraper.getCoursesChrome', studioid, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Finished single scrape');
+        }
+      });
+    }
+  },
+  'submit .add-from-json'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+    const jsonfile = (event.target.jsonfile.value);
+    if (jsonfile !== '') {
+      Meteor.call('coursescraper.addCoursesFromFile', jsonfile, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Finished adding courses');
         }
       });
     }
