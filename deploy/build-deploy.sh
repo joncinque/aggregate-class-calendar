80e01755d8143d272beb3c289ec23ae8e3cf3155
Jon Cinque <jon.cinque@gmail.com>
2017-07-28 21:57:21 +0100

    Cleanup from deployment

--- deploy/build-deploy.sh
+++ deploy/build-deploy.sh
@@ -2,4 +2,4 @@
 meteor build ../app --architecture os.linux.x86_64
 sshpass -p $PASSWORD scp ../app/aggregate-class-calendar.tar.gz aggregate@$IP:/home/aggregate
 sshpass -p $PASSWORD ssh aggregate@$IP tar -xvzf aggregate-class-calendar.tar.gz
-sshpass -p $PASSWORD ssh aggregate@$IP "echo $PASSWORD | sudo -S supervisorctl update aggregate-class-calendar"
+echo $PASSWORD | sshpass -p $PASSWORD ssh aggregate@$IP sudo -S supervisorctl update aggregate-class-calendar
